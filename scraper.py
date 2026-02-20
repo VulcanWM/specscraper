5154415ff0fa51b03f1a77ed28ae4becc4974847
VulcanWM <logmedha09@gmail.com>
2026-02-20 09:11:05 +0000

    figured out the problem to the bug

--- scraper.py
+++ scraper.py
@@ -24,31 +24,41 @@ for link in links:
 
 print(topics)
 
-# for topic in topics:
-#     topic_url = topics[topic]
-#     topic_page = requests.get(topic_url)
-#     subtopics = []
-#
-#     soup = BeautifulSoup(topic_page.content, "html.parser")
-#     results = soup.find_all("table")
-#     h3s = soup.find_all("h3", class_="mb-5 text-[color:var(--subject-color-primary)]")
-#     for h3 in h3s:
-#         subtopics.append(h3.get_text())
-#         output[topic_name][h3.get_text()] = {}
-#     print(json.dumps(output, indent=4))
-#
-#     subtopicIndex = 0
-#     for result in results:
-#         tbody = result.find_all("tbody")[0]
-#         tds = result.find_all("td")
-#         count = 0
-#         for td in tds:
-#             if count == 0:
-#                 output[topic_name][subtopics[subtopicIndex]]['content'] = td.get_text().replace(".", ". ")
-#             elif count == 1:
-#                 output[topic_name][subtopics[subtopicIndex]]['opportunity'] = td.get_text().replace(".", ". ")
-#             count += 1
-#         subtopicIndex += 1
+# second subtopic isn't working because it uses h4 because of even more subtopics
+# maybe count if the number of h3s is equal to tables
+# if yes then only use have subtopics, if not then there are subsubtopics
+
+for topic in topics:
+    topic_url = topics[topic]
+    topic_page = requests.get(topic_url)
+    subtopics = []
+
+    soup = BeautifulSoup(topic_page.content, "html.parser")
+    results = soup.find_all("table")
+    h3s = soup.find_all("h3", class_="mb-5 text-[color:var(--subject-color-primary)]")
+    h4s = soup.find_all("h4", class_="mb-3 text-[color:var(--subject-color-primary)]")
+    if len(h3s) == len(results):
+        print("subtopics")
+    elif len(h4s) == len(results):
+        print("subsubtopics")
+    for h3 in h3s:
+        subtopics.append(h3.get_text())
+        output[topic][h3.get_text()] = {}
+    print(subtopics)
+    print(json.dumps(output, indent=4))
+
+    # subtopicIndex = 0
+    # for result in results:
+    #     tbody = result.find_all("tbody")[0]
+    #     tds = result.find_all("td")
+    #     count = 0
+    #     for td in tds:
+    #         if count == 0:
+    #             output[topic][subtopics[subtopicIndex]]['content'] = td.get_text().replace(".", ". ")
+    #         elif count == 1:
+    #             output[topic][subtopics[subtopicIndex]]['opportunity'] = td.get_text().replace(".", ". ")
+    #         count += 1
+    #     subtopicIndex += 1
 
 
 # then should get the name of each link (topic), then get the subtopic using the code below
